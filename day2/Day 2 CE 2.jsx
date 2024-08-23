a000808d3b3af42fce2189e5521a3027fd48a79c
janagan0920 <727723eucs083@skcet.ac.in>
2024-08-23 10:03:19 +0530

    Add files via upload

---
+++ day2/Day 2 CE 2.jsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    
+      <div style={{backgroundColor:"Lightblue",fontSize:'10px',border:'1px solid blue',borderRadius:'5px'}}>
+        <h1 style={{color: "green"}}>Inline Style in JSX Example.</h1>
+        <p style={{color: "darkblue",fontSize:'16px'}}>This is a paragraph with inline styles applied.</p>
+      </div>
+  );
+}
+
+export default App;
